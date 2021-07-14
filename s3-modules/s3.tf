7b70bd1dd62452618394cc1b42dd64884df21e02
Daniel Gamboa Estrada <dgamboaestrada@gmail.com>
2021-07-14 18:45:00 -0500

    Use git as source module

--- s3-modules/s3.tf
+++ s3-modules/s3.tf
@@ -1,5 +1,5 @@
 module "bucket" {
-  source="./modules/s3"
+  source = "git::git@github.com:dgamboaestrada/terraform-workshop.git//s3-modules/modules/s3?ref=main"
   name = "${local.s3_prefix}-test1"
   tags = {
     Project     = "terraform-workshop"
